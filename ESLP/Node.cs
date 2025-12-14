ec4de816bda8e5db24d415d472ef0efec58e27ad
asiryan <dmc5mod@yandex.ru>
2025-12-15 02:04:11 +0300

    Refactoring

--- ESLP/Node.cs
+++ ESLP/Node.cs
@@ -5,18 +5,11 @@ namespace ESLP
     // ============================================================
     //                WORKER STATE (custom heap)
     // ============================================================
-    struct Node
+    [method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+    struct Node(UInt192 s, int a, int packed)
     {
-        public UInt192 Sum;
-        public int A;
-        public int Packed;  // [31..24]=r, [23..0]=idx
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Node(UInt192 s, int a, int packed)
-        {
-            Sum = s;
-            A = a;
-            Packed = packed;
-        }
+        public UInt192 Sum = s;
+        public int A = a;
+        public int Packed = packed;  // [31..24]=r, [23..0]=idx
     }
 }
